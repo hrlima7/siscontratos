495698927565d804b6a5c248b0a169febb878793
Henrique roma <hrlima7@gmail.com>
2021-04-10 19:20:37 -0300

    Commit inicial

---
+++ src/main/java/br/com/roma/ContratoApplication.java
@@ -0,0 +1,13 @@
+package br.com.roma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContratoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/roma/ContratoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
